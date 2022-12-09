afe3d44a96e5f1896c1c4f18087476dc4f7a7151
unknown <baselsalah2053@gmail.com>
2022-12-09 19:36:47 +0200

    serach contacts

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,21 +1,52 @@
 import propTypes from 'prop-types';
+import { useState } from 'react';
 
 const ListContacts = props => {
+	const [query, setQuery] = useState('');
+	const contactSearchHandler = e => {
+		setQuery(e.target.value);
+	};
+
+	const clearQueryHandler = () => {
+		setQuery('');
+	};
+
+	const showContacts =
+		query === '' ? props.contacts : props.contacts.filter(c => c.name.toLowerCase().includes(query.toLowerCase()));
 	return (
-		<ol className='contact-list'>
-			{props.contacts.map(contact => (
-				<li key={contact.id} className='contact-list-item'>
-					<div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }}></div>
-					<div className='contact-details'>
-						<p>{contact.name}</p>
-						<p>{contact.handle}</p>
-					</div>
-					<button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>
-						Remove
-					</button>
-				</li>
-			))}
-		</ol>
+		<div className='list-contacts'>
+			<div className='list-contacts-top'>
+				<input
+					type='search'
+					placeholder='Search Contacts'
+					className='search-contacts'
+					value={query}
+					onChange={contactSearchHandler}
+				/>
+			</div>
+			{showContacts.length !== props.contacts.length && (
+				<div className='showing-contacts'>
+					<span>
+						Now showing {showContacts.length} of {props.contacts.length}
+					</span>
+					<button onClick={clearQueryHandler}>Show all</button>
+				</div>
+			)}
+			<ol className='contact-list'>
+				{showContacts.map(contact => (
+					<li key={contact.id} className='contact-list-item'>
+						<div className='contact-avatar' style={{ backgroundImage: `url(${contact.avatarURL})` }}></div>
+						<div className='contact-details'>
+							<p>{contact.name}</p>
+							<p>{contact.handle}</p>
+						</div>
+						<button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>
+							Remove
+						</button>
+					</li>
+				))}
+			</ol>
+		</div>
 	);
 };
 
