018d5c5dc06b9f310481401773d77e55dd9476da
unknown <baselsalah2053@gmail.com>
2022-12-14 04:10:00 +0200

    contact form

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,21 @@ import '../css/App.css';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact';
 import * as ContactsAPI from '../utils/ContactsAPI';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 const App = () => {
+	let navigate = useNavigate();
 	const removeContact = contact => {
 		ContactsAPI.remove(contact);
 		setContacts(contacts.filter(c => c.id !== contact.id));
+	};
+	const createContact = contact => {
+		const create = async () => {
+			const res = await ContactsAPI.create(contact);
+			setContacts(prevState => [...prevState, res]);
+			// setContacts(contacts.contact(res));
+		};
+		create();
+		navigate('/'); // after creating a new contact we return user to root path
 	};
 	const [contacts, setContacts] = useState([]);
 	useEffect(() => {
@@ -20,7 +30,17 @@ const App = () => {
 	return (
 		<Routes>
 			<Route exact path='/' element={<ListContacts contacts={contacts} onDeleteContact={removeContact} />} />
-			<Route path='/create' element={<CreateContact />} />
+			{/* <Route
+				path='/create'
+				element={
+					<CreateContact
+						onCreateContact={contact => {
+							createContact(contact);
+						}}
+					/>
+				}
+			/> */}
+			<Route path='/create' element={<CreateContact onCreateContact={createContact} />} />
 		</Routes>
 	);
 };
--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,13 +1,22 @@
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serialize from 'form-serialize';
 
-const CreateContact = () => {
+const CreateContact = ({ onCreateContact }) => {
+	const handleSubmit = e => {
+		e.preventDefault();
+		const values = serialize(e.target, { hash: true });
+		console.log(values);
+		if (onCreateContact) {
+			onCreateContact(values);
+		}
+	};
 	return (
 		<div>
 			<Link to='/' className='close-create-contact'>
 				Close
 			</Link>
-			<form className='create-contact-form'>
+			<form className='create-contact-form' onSubmit={handleSubmit}>
 				<ImageInput className='create-contact-avatar-input' name='avatarURL' maxHeight={64} />
 				<div className='create-contact-details'>
 					<input type='text' name='name' placeholder='Name' />
